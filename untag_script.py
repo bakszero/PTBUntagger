5fa09ad7a6b75b5a31a8deb2001a8622ba0e51f3
Bakhtiyar Syed <syed.b@research.iiit.ac.in>
2016-08-24 23:31:11 +0530

    Removed unnecessary bracket in re.findall function

--- untag_script.py
+++ untag_script.py
@@ -4,7 +4,7 @@ import sys, re
 
 with open(sys.argv[1], "r") as filestream:
 	for line in filestream:
-		array = re.findall('[\b[A-Za-z,0-9\.]+/', line)
+		array = re.findall('[\bA-Za-z,0-9\.]+/', line)
 		for word in array:
 			final=re.findall('[A-Za-z,0-9\.]+', word)
 			for token in final:
